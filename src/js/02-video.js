9c4a1f69b4215e0d4b7d8fb70d9162058cdf2868
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-05-26 21:01:55 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time') || 0
+);
+
+player.setCurrentTime(currentTime);
