1db36380488916416479d8a10f2c9448c0ba34d3
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-05-27 16:27:16 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+import storage from './localstorage';
 
+const formEl = document.querySelector('.feedback-form');
+
+let feedbackFormInfo = {};
+const dataFromStorage = storage.load('feedback-form-state');
+
+const fillFormFields = () => {
+  if (dataFromStorage) {
+    for (const key in dataFromStorage) {
+      formEl.elements[key].value = dataFromStorage[key];
+    }
+  } else return;
+};
+
+fillFormFields();
+
+function onFormFieldTypeText(event) {
+  const formFieldName = event.target.name;
+  const formFieldValue = event.target.value.trim();
+
+  feedbackFormInfo[formFieldName] = formFieldValue;
+  storage.save('feedback-form-state', feedbackFormInfo);
+}
+
+function onFormFieldSubmit(event) {
+  event.preventDefault();
+  console.log(feedbackFormInfo);
+
+  event.target.reset();
+  storage.remove('feedback-form-state');
+  feedbackFormInfo = {};
+}
+
+formEl.addEventListener('input', throttle(onFormFieldTypeText, 500));
+formEl.addEventListener('submit', onFormFieldSubmit);
